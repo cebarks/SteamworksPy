592614c9862f340d5a66382d838d7a24962cd154
Anten Skrabec <cebarks@gmail.com>
2025-12-25 03:20:07 -0700

    implement changes from #91 without the formatting changes included in the original

--- steamworks/structs.py
+++ steamworks/structs.py
@@ -45,6 +45,24 @@ class GetAppDependenciesResult(Structure):
         return dependencies_list
 
 
+class GetAppDependenciesResult(Structure):
+    _fields_ = [
+        ("result", c_int32),
+        ("publishedFileId", c_uint64),
+        ("array_app_dependencies", POINTER(c_int32)),
+        ("array_num_app_dependencies", c_int32),
+        ("total_num_app_dependencies", c_int32)
+    ]
+
+    def get_app_dependencies_list(self) -> list:
+        dependencies_list = []
+        array_size = self.array_num_app_dependencies
+        array_type = c_uint32 * array_size
+        array = array_type.from_address(addressof(self.array_app_dependencies.contents))
+        dependencies_list.extend(array)
+        return dependencies_list
+
+
 class SubscriptionResult(Structure):
     _fields_ = [("result", c_int32), ("publishedFileId", c_uint64)]
 
